10f5fb61394324122aecf650a8b2ab4dbad7e437
glaubert <suyann12@hotmail.com>
2022-07-07 14:21:04 -0300

    :tada: created getStringParser function

--- __init__.py
+++ __init__.py
@@ -1,25 +1,32 @@
 from classes.code import CODE
 
 if __name__ == "__main__":
+
     '''
         This is a string to be encrypted
     '''
     string_to_encrypt = "agua"
+    default_parser = "/"
 
     # Encrypt process
-    string_encrypted = CODE.encodeString(string_to_encrypt)
+    string_encrypted = CODE.encodeString(string_to_encrypt, default_parser)
 
     # descrypting process
-    string_decrypted = CODE.decodeString(string_encrypted)
+    string_decrypted = CODE.decodeString(string_encrypted, default_parser)
 
+    string_parser = CODE.getStringParser("twjuzltmvzmxznhiut%jzorrcgqokdkbvYWd1YQ==")
 
     #Testing results
     print("""
             String Original: {}
             Encrypted: {}
-            Decrypted: {}""". format(
+            Decrypted: {}
+            Parser: {}
+            Caracter: {}""". format(
                         string_to_encrypt,
                         string_encrypted,
-                        string_decrypted
+                        string_decrypted,
+                        string_parser,
+                        chr(61)
                     )
         )
